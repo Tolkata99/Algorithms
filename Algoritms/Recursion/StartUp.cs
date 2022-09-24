b186b4e1c800605ff11ab4a6368e4b7ac1d12dd4
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-09-24 11:15:27 +0300

    Add Recursive Factorial

--- Algoritms/Recursion/StartUp.cs
+++ Algoritms/Recursion/StartUp.cs
@@ -1,13 +1,13 @@
 ﻿
 
-public static class StartUp
+public class StartUp
 {
     public static void Main(string[] args)
     {
-         int[] n = Console.ReadLine()
-                         .Split(" ")
-                         .Select(int.Parse)
-                         .ToArray();
+        int[] n = Console.ReadLine()
+                        .Split(" ")
+                        .Select(int.Parse)
+                        .ToArray();
 
         int sum = Recursion(n, 0);
 
@@ -17,9 +17,9 @@ public static class StartUp
 
     public static int Recursion(int[] arr, int startIndex)
     {
-        if(startIndex == arr.Length - 1)
+        if (startIndex == arr.Length - 1)
         {
-            return arr[startIndex]; 
+            return arr[startIndex];
         }
 
         return arr[startIndex] + Recursion(arr, startIndex + 1);
