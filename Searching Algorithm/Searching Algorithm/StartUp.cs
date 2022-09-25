e0bca7852c54ceb81de63bc1a0876b1fc9926bd3
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-09-25 14:27:10 +0300

    Binary search
    Add binary search method

---
+++ Searching Algorithm/Searching Algorithm/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace Searching_Algorithm
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 1, 2, 3 };
+            //linear search
+            //binary search == ordered data !!!!!
+
+            Console.WriteLine(BinarySearch(arr, 1, 0, 2));
+        }
+
+        public static int BinarySearch(int[] arr, int s, int l, int r)
+        {
+            while (r >= l)
+            {
+                int midIndex = (l + r) / 2;
+
+                if (s > arr[midIndex])
+                {
+                    l = midIndex + 1;
+                }
+                else if (s < arr[midIndex])
+                {
+                    r = midIndex - 1;
+                }
+                else
+                {
+                    return midIndex;    
+                }
+
+               
+            }
+
+            return -1;
+        }
+    }
+}
